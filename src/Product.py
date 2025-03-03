49a761e0758088e6ac22dd5b27f6950ae165e1ea
35347 <353472010401150@mail.ru>
2025-03-03 22:01:42 +0300

    yes

--- src/Product.py
+++ src/Product.py
@@ -1,12 +1,9 @@
-"""Класс для работы с товарами"""
+from src.BaseProduct import BaseProduct
 
-
-class Product:
+class Product(BaseProduct):
     def __init__(self, name, description, price, quantity):
-        self.name = name
-        self.description = description
+        super().__init__(name, description, price, quantity)
         self.__price = price  # Приватный атрибут
-        self.quantity = quantity
 
     @property
     def price(self):
@@ -16,26 +13,10 @@ class Product:
     def price(self, value):
         if value <= 0:
             raise ValueError("Цена не должна быть нулевая или отрицательная")
-        elif hasattr(self, "_price") and value < self.__price:
-            confirm = input("Вы уверены, что хотите понизить цену? (y/n): ")
-            if confirm.lower() == "y":
-                self.__price = value
-            else:
-                print("Цена не изменена.")
-        else:
-            self.__price = value
+        self.__price = value
 
     def __str__(self):
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
-
-    def __repr__(self):
-        return f"{self.name}, описание: {self.description}, цена = {self.price}, количество = {self.quantity}"
-
-    def __add__(self, other):
-        if type(self) is not type(other):
-            raise TypeError("Нельзя складывать продукты разных типов.")
-        return (self.price * self.quantity) + (other.price * other.quantity)
-
 
 class Smartphone(Product):
     def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
@@ -44,7 +25,6 @@ class Smartphone(Product):
         self.model = model
         self.memory = memory
         self.color = color
-
 
 class LawnGrass(Product):
     def __init__(self, name, description, price, quantity, country, germination_period, color):
